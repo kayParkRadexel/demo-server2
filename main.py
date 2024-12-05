622daec0f02f663fabef93a1a8550e6dd639fa32
Kay Park <kaypark@radexel.com>
2024-12-05 09:05:29 +0900

    dd

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World, this is server 2"}
+
+
+@app.get("/be2", status_code=200)
+async def be2():
+    return {"message": "Hello World, this is server 2"}
