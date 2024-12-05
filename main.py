afe57f801955c1edc3dd368b331895acb3a53d51
Kay Park <kaypark@radexel.com>
2024-12-05 15:56:47 +0900

    path modi

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/service2/")
 async def root():
     return {"message": "Hello World, this is server 2"}
 
 
-@app.get("/be2", status_code=200)
+@app.get("/service2/be2", status_code=200)
 async def be2():
     return {"message": "Hello World, this is server 2"}
