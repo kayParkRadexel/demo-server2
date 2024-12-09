55ff98d64240b7253b134ae7200c0a036ff8814e
Kay Park <kaypark@radexel.com>
2024-12-09 10:45:18 +0900

    upda

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/service2/")
+@app.get("/")
 async def root():
-    return "Hello World, this is server 2"
-
-
-@app.get("/service2/be2", status_code=200)
-async def be2():
     return "Hello World, this is server 2"
